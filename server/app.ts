01b2f9e471e7656737b55413f32d1b90bf8db088
bismay <bismaypurkayastha5467@gmail.com>
2024-02-22 20:30:21 +0530

    converting the express app to serverless function

--- server/app.ts
+++ server/app.ts
@@ -15,67 +15,52 @@ dotenv.config({
       : './.env.development.local',
 });
 
-function server() {
-  const app = express();
-  // eslint-disable-next-line no-magic-numbers
-  const PORT = Number(process.env.PORT) || 3001;
+const app = express();
+// eslint-disable-next-line no-magic-numbers
+const PORT = Number(process.env.PORT) || 3001;
 
-  process.on('uncaughtException', (error) => {
-    console.error('UNCAUGHT EXCEPTION! Shutting down...');
-    console.error(error.name, error.message);
-    process.exit(1);
-  });
+process.on('uncaughtException', (error) => {
+  console.error('UNCAUGHT EXCEPTION! Shutting down...');
+  console.error(error.name, error.message);
+  process.exit(1);
+});
 
-  app.use(
-    cors({
-      origin: process.env.CLIENT_URL,
-      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-      preflightContinue: false,
-      optionsSuccessStatus: 204,
-    })
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  })
+);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(compression({ level: 6, threshold: 1000 }));
+
+app.get('/', (_req, res: Response) => {
+  res.status(SUCCESS_CODES.OK).json({ message: 'This is a get request' });
+});
+
+app.all('*', (req: Request, _res: Response, next: NextFunction) => {
+  next(
+    new ErrorHandler(
+      `Route ${req.originalUrl} not found!`,
+      ERROR_CODES['NOT FOUND']
+    )
   );
-  app.use(express.json());
-  app.use(express.urlencoded({ extended: true }));
-  app.use(cookieParser());
-  app.use(compression({ level: 6, threshold: 1000 }));
+});
 
-  app.get('/', (_req, res: Response) => {
-    res.status(SUCCESS_CODES.OK).json({ message: 'This is a get request' });
-  });
+app.use(globalErrorHandler);
 
-  app.all('*', (req: Request, _res: Response, next: NextFunction) => {
-    next(
-      new ErrorHandler(
-        `Route ${req.originalUrl} not found!`,
-        ERROR_CODES['NOT FOUND']
-      )
-    );
-  });
+console.error(process.env.NODE_ENV);
 
-  app.use(globalErrorHandler);
+// eslint-disable-next-line no-console
+if (process.env.NODE_ENV.trim() === 'production') module.exports = app;
+else app.listen(PORT, () => console.log(`Server listening on port ${PORT}🚀`));
 
-  // eslint-disable-next-line no-console
-  app.listen(PORT, () => console.log(`Server listening on port ${PORT}🚀`));
-
-  process.on('unhandledRejection', (error: Error) => {
-    console.error('UNHANDLED REJECTION! Shutting down...');
-    console.error(error.name, error.message);
-    process.exit(1);
-  });
-}
-
-(function () {
-  if (process.env.NODE_ENV.trim() === 'production') {
-    if (cluster.isPrimary) {
-      const noOfCores = Number(process.env.NUMBER_OF_PROCESSORS);
-      for (let i = 1; i <= noOfCores; i++) cluster.fork();
-
-      cluster.on('exit', (worker) => {
-        // eslint-disable-next-line no-console
-        console.log(`Worker ${worker.id} died...Spawning a new cluster...`);
-        cluster.fork();
-      });
-    }
-    if (cluster.isWorker) server();
-  } else server();
-})();
+process.on('unhandledRejection', (error: Error) => {
+  console.error('UNHANDLED REJECTION! Shutting down...');
+  console.error(error.name, error.message);
+  process.exit(1);
+});
