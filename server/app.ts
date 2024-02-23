dafaf3bf7006bc093ad3f8518878bd70c1f7393b
bismay <bismaypurkayastha5467@gmail.com>
2024-02-23 19:08:57 +0530

    adding cors

--- server/app.ts
+++ server/app.ts
@@ -1,7 +1,11 @@
+import compression from 'compression';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
 import dotenv from 'dotenv';
-import express, { Express, Request, Response } from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 
-import { SUCCESS_CODES } from './constants/statusCode';
+import { ERROR_CODES, SUCCESS_CODES } from './constants/statusCode';
+import { ErrorHandler, globalErrorHandler } from './utils/errorHandler';
 
 dotenv.config({
   path:
@@ -10,9 +14,28 @@ dotenv.config({
       : './.env.development.local',
 });
 
-const app: Express = express();
+const app = express();
 // eslint-disable-next-line no-magic-numbers
 const PORT = Number(process.env.PORT) || 3001;
+
+process.on('uncaughtException', (error) => {
+  console.error('UNCAUGHT EXCEPTION! Shutting down...');
+  console.error(error.name, error.message);
+  process.exit(1);
+});
+
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: SUCCESS_CODES['NO CONTENT'],
+  })
+);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(compression({ level: 6, threshold: 1000 }));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -21,7 +44,24 @@ app.get('/', (_req: Request, res: Response) => {
   res.status(SUCCESS_CODES.OK).json({ message: 'This is a get request' });
 });
 
+app.all('*', (req: Request, _res: Response, next: NextFunction) => {
+  next(
+    new ErrorHandler(
+      `Route ${req.originalUrl} not found!`,
+      ERROR_CODES['NOT FOUND']
+    )
+  );
+});
+
+app.use(globalErrorHandler);
+
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`LOG: Server listening on PORT ${PORT}`);
+});
+
+process.on('unhandledRejection', (error: Error) => {
+  console.error('UNHANDLED REJECTION! Shutting down...');
+  console.error(error.name, error.message);
+  process.exit(1);
 });
