a82bd49905b7955751ac0d2f9b1fd0ae98a7a080
bismay <bismaypurkayastha5467@gmail.com>
2024-03-11 05:13:40 +0530

    changes made

--- server/router/index.ts
+++ server/router/index.ts
@@ -1,9 +1,13 @@
+import adminRouter from './admin/router';
 import authRouter from './auth/router';
 import bookmarkRouter from './bookmark/router';
 import commentRouter from './filePreview/comments/router';
 import fileRouter from './filePreview/data/router';
 import folderRoute from './folder/router';
+import reportRouter from './report/router';
 import { router } from '../config/trpcConfig';
+import searchRouter from './search/router';
+import uploadRoute from './upload/router';
 
 export const appRouter = router({
   comment: commentRouter,
@@ -11,6 +15,10 @@ export const appRouter = router({
   auth: authRouter,
   folder: folderRoute,
   bookmark: bookmarkRouter,
+  report: reportRouter,
+  upload: uploadRoute,
+  admin: adminRouter,
+  search: searchRouter,
 });
 
 export type AppRouter = typeof appRouter;
