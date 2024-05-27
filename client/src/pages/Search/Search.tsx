a94f0f1cec89b902cc0cb38c3f8e73cd1c0decaa
simply_ark0 <arkojeetbera@simply-arko.local>
2024-05-27 22:16:27 +0530

    Implemented Infinite Scrolling with useSWRInfinite

--- client/src/pages/Search/Search.tsx
+++ client/src/pages/Search/Search.tsx
@@ -1,17 +1,17 @@
 import ResultCard from './ResultCard/ResultCard';
-import {} from '@/components/ui/drawer';
 import DrawerFilter from './Filter/DrawerFilter';
 import AsideFilter from './Filter/AsideFilter';
 import { Link, useSearchParams } from 'react-router-dom';
-import { useState } from 'react';
-import { useSearch } from '@/hooks/swr-hooks/useSearch';
+import { useEffect } from 'react';
 import { Button } from '@/components/ui/button';
+import useSWRInfinite, { SWRInfiniteKeyLoader } from 'swr/infinite';
+import { SearchResponse } from '@/types/api-types';
+import { axiosInstance } from '@/utils/swr/fetcher';
 
 export default function Search() {
-  const [page, setPage] = useState<number>(1);
   const [searchParams, _] = useSearchParams();
 
-  const getParamsPayload = () => {
+  const fetcher = (url: string) => {
     const entries = {} as Record<string, string | string[]>;
     searchParams.forEach((value, key) => {
       if (key === 'year') {
@@ -21,12 +21,21 @@ export default function Search() {
       } else if (key) entries[key] = value;
     });
 
-    entries['page'] = String(page);
-
-    return entries;
+    return axiosInstance({ url: url, params: entries, method: 'GET' });
   };
 
-  const { data } = useSearch(getParamsPayload());
+  const getKey: SWRInfiniteKeyLoader = (pageIndex, previousPageData) => {
+    // TODO: Content of previousPageData needs further testing
+
+    if (previousPageData && !previousPageData.hasMore) return null;
+    return `/search?page=${pageIndex + 1}`;
+  };
+
+  const { data, setSize, mutate } = useSWRInfinite(getKey, fetcher);
+
+  useEffect(() => {
+    mutate();
+  }, [searchParams]);
 
   return (
     <div className="max-w-[1280px] mx-auto min-h-[400px] sm:grid sm:grid-cols-10 sm:gap-x-4">
@@ -35,20 +44,25 @@ export default function Search() {
       </div>
       <AsideFilter />
       <div className="p-4 flex flex-col gap-y-4 sm:col-span-7">
-        {data?.data.map((res) => (
-          <Link to={`/preview/${res._id}`} target="_blank" key={res._id}>
-            <ResultCard
-              id={res._id}
-              instituteName={res.institutionName}
-              semester={res.semester}
-              subjectCode={res.semester}
-              year={res.year}
-            />
-          </Link>
-        ))}
+        {data?.map((value) => {
+          const data: SearchResponse = value.data;
+          return data?.data.map((res) => {
+            return (
+              <Link to={`/preview/${res._id}`} target="_blank" key={res._id}>
+                <ResultCard
+                  id={res._id}
+                  instituteName={res.institutionName}
+                  semester={res.semester}
+                  subjectCode={res.semester}
+                  year={res.year}
+                />
+              </Link>
+            );
+          });
+        })}
         <Button
           onClick={() => {
-            data?.hasMore && setPage((prev) => prev + 1);
+            setSize((prev) => prev + 1);
           }}
         >
           Load More
--- client/src/utils/swr/fetcher.ts
+++ client/src/utils/swr/fetcher.ts
@@ -1,8 +1,11 @@
-import axios from 'axios';
+import axios, { AxiosRequestConfig } from 'axios';
 
 export const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_BASE_URL,
 });
 
-const fetcher = (url: string) => axiosInstance.get(url).then((res) => res.data);
+const fetcher = async (req: AxiosRequestConfig<any>) => {
+  const res = await axiosInstance(req);
+  return res;
+};
 export default fetcher;
