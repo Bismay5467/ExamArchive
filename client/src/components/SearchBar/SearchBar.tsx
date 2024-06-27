9ff7d26dd86330151ec562c927144909224beca6
bismay <bismaypurkayastha5467@gmail.com>
2024-06-27 20:59:29 +0530

    minnor bug fix

--- client/src/components/SearchBar/SearchBar.tsx
+++ client/src/components/SearchBar/SearchBar.tsx
@@ -1,5 +1,5 @@
 import { Input } from '@nextui-org/react';
-import debounce from 'lodash.debounce';
+// import debounce from 'lodash.debounce';
 import { IoIosSearch } from 'react-icons/io';
 import { useEffect, useState } from 'react';
 import { useSearch } from '@/hooks/useSearch';
@@ -8,14 +8,13 @@ export default function SearchBar({ className }: { className?: string }) {
   const { setSearchParam, searchInputs } = useSearch();
   const [searchField, setSearchField] = useState<string>('');
 
-  const DELAY_IN_MS = 500;
-  const debouncedSearch = debounce(
-    (query: string) => setSearchParam(query),
-    DELAY_IN_MS
-  );
+  // const DELAY_IN_MS = 500;
+  // const debouncedSearch = (query: string) => {
+  //   setSearchParam(query);
+  // };
   const handleSearch = (query: string) => {
     setSearchField(query);
-    debouncedSearch(query);
+    setSearchParam(query);
   };
   useEffect(() => {
     if (searchInputs.searchParams) setSearchField(searchInputs.searchParams);
