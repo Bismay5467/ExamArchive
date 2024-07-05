851114627bce03e30ca62bf2934b82307fa567a1
bismay <bismaypurkayastha5467@gmail.com>
2024-07-05 06:09:24 +0530

    lint fix

--- client/src/pages/Preview/PreviewContent/TagsSection/TagsSection.tsx
+++ client/src/pages/Preview/PreviewContent/TagsSection/TagsSection.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable jsx-a11y/no-static-element-interactions */
-/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
+
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 import {
   Modal,
