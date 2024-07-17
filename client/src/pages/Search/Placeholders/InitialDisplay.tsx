db86a00ab184e533c9c22d0dc111a0ec277738ab
bismay <bismaypurkayastha5467@gmail.com>
2024-07-17 16:31:13 +0530

    img rendering optimised

--- client/src/pages/Search/Placeholders/InitialDisplay.tsx
+++ client/src/pages/Search/Placeholders/InitialDisplay.tsx
@@ -1,4 +1,7 @@
+import { useState } from 'react';
+
 export default function InitialDisplay() {
+  const [loaded, setLoaded] = useState<boolean>(false);
   return (
     <div className="flex flex-col gap-y-3 font-natosans text-lg justify-center items-center">
       <img
@@ -7,13 +10,18 @@ export default function InitialDisplay() {
         src="https://res.cloudinary.com/dzorpsnmn/image/upload/v1719864150/EXAM-ARCHIVE-ASSETS/w294za0tvklwsrustgwn.jpg"
         // fallbackSrc="https://via.placeholder.com/300x200"
         alt="Search"
+        onLoad={() => setLoaded(true)}
       />
-      <p className="text-slate-800 text-sm text-center sm:text-medium">
-        Enter comma seperated tags to see relevant results here
-      </p>
-      <p className="text-slate-500 text-sm text-center sm:text-medium">
-        For example : paging, segmentation
-      </p>
+      {loaded === true && (
+        <>
+          <p className="text-slate-800 text-sm text-center sm:text-medium">
+            Enter comma seperated tags to see relevant results here
+          </p>
+          <p className="text-slate-500 text-sm text-center sm:text-medium">
+            For example : paging, segmentation
+          </p>
+        </>
+      )}
     </div>
   );
 }
--- client/src/pages/Search/Placeholders/NoResults.tsx
+++ client/src/pages/Search/Placeholders/NoResults.tsx
@@ -1,6 +1,9 @@
 /* eslint-disable react/no-unescaped-entities */
 
+import { useState } from 'react';
+
 export default function NoResults() {
+  const [loaded, setloaded] = useState<boolean>(false);
   return (
     <div className="flex flex-col gap-y-3 font-natosans text-lg justify-center items-center">
       <img
@@ -8,13 +11,18 @@ export default function NoResults() {
         height={300}
         src="https://res.cloudinary.com/dzorpsnmn/image/upload/v1719865549/EXAM-ARCHIVE-ASSETS/fdixjd7edbsqna7jow4c.jpg"
         alt="Not found"
+        onLoad={() => setloaded(true)}
       />
-      <p className="text-slate-800 text-sm text-center sm:text-medium">
-        Couldn't find any results with the specified tag(s)
-      </p>
-      <p className="text-slate-500 text-sm text-center sm:text-medium">
-        Try searching for something else, or try with a different spelling
-      </p>
+      {loaded === true && (
+        <>
+          <p className="text-slate-800 text-sm text-center sm:text-medium">
+            Couldn't find any results with the specified tag(s)
+          </p>
+          <p className="text-slate-500 text-sm text-center sm:text-medium">
+            Try searching for something else, or try with a different spelling
+          </p>
+        </>
+      )}
     </div>
   );
 }
